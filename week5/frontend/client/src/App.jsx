77833b347ac751abe9374c10a9289dd9de09c6de
Adityak087 <Adityak@thebreapps.com>
2024-06-07 21:06:48 +0530

    three

--- week5/frontend/client/src/App.jsx
+++ week5/frontend/client/src/App.jsx
@@ -3,33 +3,37 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Aditya(){
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <card className = "card">
+        <li>Hi</li>
+        <li>Hi</li>
+        <li>Hi</li>
+      </card>
     </>
   )
 }
+
+function Prajyot(){
+  return( 
+    <>
+      <div>
+        <h1>Hi Prajyot</h1>
+      </div>
+    </>
+  )
+}
+
+function App() {
+  return (
+    <>
+      <Aditya></Aditya>
+      <Prajyot></Prajyot>
+    </>
+  )
+}
+
+
 
 export default App
